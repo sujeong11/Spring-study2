bb4dd45d1c500c29fca28cc6f23722eb2b30a08a
sujeong11 <qalzm7351@gmail.com>
2022-07-30 19:21:34 +0900

    문제해결) JSR-330 Provider 사용

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -39,10 +40,10 @@ public class SingletonWithPrototypeTest1 {
     static class ClientBean {
 
         @Autowired // 이 방법보다는 생성자로 주입하는 것이 좋다.
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getIfUnique();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
